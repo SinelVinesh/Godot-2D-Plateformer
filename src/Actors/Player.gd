01918bfe4ab535d3c32266422c854450610c4b16
SoleilDivin <vinesh.sinel@gmail.com>
2023-02-13 14:15:17 +0300

    Feat: Add movement to the player

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,3 +1,8 @@
 extends Actor
 
-
+func _physics_process(delta) -> void:
+	var direction: = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		0.0        
+	)
+	velocity = speed * direction
