cf3a289b9792759b795654199f84ef7d3b511206
SoleilDivin <vinesh.sinel@gmail.com>
2023-02-13 14:41:02 +0300

    feat: Allow player to jump

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,8 +1,28 @@
 extends Actor
 
 func _physics_process(delta) -> void:
-	var direction: = Vector2(
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculte_move_velocity(_velocity, speed, direction, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity,UP_DIRECTION)
+
+func get_direction() -> Vector2:
+	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		0.0        
+		-1.0 if Input.is_action_pressed("jump") and is_on_floor() else 1.0        
 	)
-	velocity = speed * direction
+
+func calculte_move_velocity(
+		linear_velocity:Vector2,
+		speed: Vector2,
+		direction: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var output: Vector2 = linear_velocity
+	output.x = speed.x * direction.x
+	output.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		output.y = speed.y * direction.y
+	if is_jump_interrupted:
+		output.y = 0.0
+	return output
