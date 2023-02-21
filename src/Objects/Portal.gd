b95d846a4c5a9c4f99dbbe2c4be4e0afe922ea99
SoleilDivin <vinesh.sinel@gmail.com>
2023-02-21 13:51:29 +0300

    Feat: Create portal to move through levels

---
+++ src/Objects/Portal.gd
@@ -0,0 +1,16 @@
+tool
+extends Node2D
+
+onready var animation_player = $Area2D/AnimationPlayer
+export var next_scene: PackedScene
+
+func _on_body_entered(_body:Node):
+	teleport()
+
+func teleport():
+	animation_player.play("Fade in")
+	yield(animation_player,"animation_finished")
+	get_tree().change_scene_to(next_scene)
+
+
+	
